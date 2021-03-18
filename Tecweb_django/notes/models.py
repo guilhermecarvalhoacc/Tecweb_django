8befcae2fc48e175349d0a4198466c3f33655c46
guilhermecarvalhoacc <guilhermeaugustochaves@hotmail.com>
2021-03-18 19:33:54 -0300

    inicio handout django

---
+++ Tecweb_django/notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    id = models.AutoField(primary_key=True)
+    def __str__(self):
+        return f"{self.id}.{self.title}"
+        
+
