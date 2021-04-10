2df6a1bc41bdcfeee633c8ccdb9ace64802b7018
guilhermecarvalhoacc <guilhermeaugustochaves@hotmail.com>
2021-04-10 20:26:53 -0300

    atualizado para editar e excluir

--- Tecweb_django/notes/views.py
+++ Tecweb_django/notes/views.py
@@ -1,5 +1,32 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Note
+
+
+def add(title,content):
+    note_obj = Note(title = title, content= content)
+    note_obj.save()
 
 
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        ID = request.POST.get("Id")
+        print(title,"\n",content,"\n",ID)
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        if ID == "":
+            add(title,content)
+        else:
+            if title == None:
+                Note.objects.filter(id=ID).delete()
+            else:
+                print("entrei no else loko")
+                edition = Note.objects.get(id=ID)
+                edition.title = title
+                edition.content = content
+                edition.save()
+        return redirect('index')
+    
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
