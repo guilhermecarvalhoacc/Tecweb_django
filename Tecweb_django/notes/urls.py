936a0e3e51fdef7423350a2ef2693852f7e27381
guilhermecarvalhoacc <guilhermeaugustochaves@hotmail.com>
2021-05-03 15:28:38 -0300

    heroku

--- Tecweb_django/notes/urls.py
+++ Tecweb_django/notes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.tags, name='tags'),
+    path('listatags', views.listatags, name='listatags'),
+  #  path('tag/<str:tag_>/', views.tag, name='tag'),
 ]
